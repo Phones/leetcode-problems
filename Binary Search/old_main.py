0ca25bacf4912ee7f1774f043e50d5fc214051f2
Phones <paulohenriqueh178h@gmail.com>
2024-02-16 22:32:52 -0300

    Binary Search apenas em python por enquanto

---
+++ Binary Search/old_main.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def __init__(self) -> None:
+        self.first_exec = True
+
+    def search(self, nums: List[int], target: int, start: int = 0, end: int = 0) -> int:
+        if self.first_exec:
+            self.first_exec = False
+            start, end = 0, len(nums) - 1
+        
+        if start == end:
+            if nums[start] == target:
+                return start
+
+            return -1
+        
+        meio = (start + end) // 2
+        if nums[meio] < target:
+            return self.search(nums, target, meio + 1, end)
+
+        return self.search(nums, target, start, meio)
+
+print(Solution().search([1,2,3,4,5,6,7,8,9,10], 8))

af0627b039c75de1ef520629a586e6e4c32a64c7
Phones <paulohenriqueh178h@gmail.com>
2024-02-20 11:18:39 -0300

    Soluções melhores para o Single Number

---
+++ Single Number/main2.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = {}
+        for num in nums:
+            result ^= num
+
+        return result
+
+
+print(Solution().singleNumber([2,2,1]))
+print(Solution().singleNumber([4,1,2,1,2]))
+print(Solution().singleNumber([1]))
+
